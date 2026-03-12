64deef2ef0905edf0dfbd74d203d2838e8f40bb4
XasanZii <169108910+XasanZii@users.noreply.github.com>
2026-03-12 05:29:03 +0300

    update files

--- pages/base_page.py
+++ pages/base_page.py
@@ -4,12 +4,19 @@ from selenium.webdriver.support import expected_conditions as EC
 from .locators import BasePageLocators, CartPageLocators
 import math
 
-
-
-class BasePage():
-    def __init__(self, browser, url):
+class BasePage:
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)  # неявное ожидание
+
+    # метод для проверки наличия элемента
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
 
     def open(self):
         self.browser.get(self.url)
